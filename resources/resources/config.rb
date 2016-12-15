5fae7d196648b89643aaf36eabe677b0328571b2
cjmateos <mateos83@gmail.com>
2016-12-15 13:25:54 +0100

    Clean resource file

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -14,9 +14,3 @@ attribute :web_dir, :kind_of => String, :default => "/var/www/rb-rails"
 attribute :memory_kb, :kind_of => Integer
 attribute :elasticache_hosts, :kind_of => Object
 attribute :zk_hosts, :kind_of => String
-
-#attribute :mystring, :kind_of => String, :default => "string example"
-#attribute :myinteger, :kind_of => Fixnum, :default => 1
-#attribute :myarray, :kind_of => Array, :default => ["val1"]
-#attribute :myhash, :kind_of => Object, :default => {"val1" => "1"}
-#attribute :myboolean, :kind_of => [TrueClass, FalseClass], :default => true
