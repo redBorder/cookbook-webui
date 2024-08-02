719180bad4ea160d1f24812eaae2272b54e299f2
Miguel Negron <manegron@redborder.com>
2024-08-02 14:02:54 +0100

    Add redborder-ai models directory

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -93,6 +93,13 @@ action :add do
       action :create
     end
 
+    directory '/var/lib/redborder-ai/models/' do
+      owner user
+      group group
+      mode '0755'
+      action :create
+    end
+
     %w(data tmp/pids tmp/delayed_job tmp/geodb public).each do |x|
       directory "/var/www/rb-rails/#{x}" do
         owner user
