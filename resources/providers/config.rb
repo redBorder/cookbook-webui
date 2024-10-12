f76c8020b239ec6b7d61fc7a6c8dc2c06b2b0252
manegron <manegron@redborder.com>
2024-10-13 00:06:21 +0100

    Testing

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -39,9 +39,9 @@ action :add do
       # notifies :run, 'bash[create_license_databag]', :delayed
       notifies :run, 'bash[db_seed]', :delayed
       notifies :run, 'bash[db_seed_modules]', :delayed
-      notifies :run, 'bash[redBorder_generate_server_key]', :delayed
+      # notifies :run, 'bash[redBorder_generate_server_key]', :delayed
       notifies :run, 'bash[redBorder_update]', :delayed
-      notifies :run, 'bash[request_trial_license]', :delayed
+      # notifies :run, 'bash[request_trial_license]', :delayed
     end
 
     dnf_package 'redborder-webui' do
@@ -54,9 +54,9 @@ action :add do
       notifies :run, 'bash[assets_precompile]', :delayed
       notifies :run, 'bash[db_seed]', :delayed
       notifies :run, 'bash[db_seed_modules]', :delayed
-      notifies :run, 'bash[redBorder_generate_server_key]', :delayed
+      # notifies :run, 'bash[redBorder_generate_server_key]', :delayed
       notifies :run, 'bash[redBorder_update]', :delayed
-      notifies :run, 'bash[request_trial_license]', :delayed
+      # notifies :run, 'bash[request_trial_license]', :delayed
     end
 
     dnf_package 'redborder-nodenvm' do
@@ -82,10 +82,9 @@ action :add do
       notifies :run, 'bash[assets_precompile]', :delayed
       notifies :run, 'bash[db_seed]', :delayed
       notifies :run, 'bash[db_seed_modules]', :delayed
+      # notifies :run, 'bash[redBorder_generate_server_key]', :delayed
       notifies :run, 'bash[redBorder_update]', :delayed
-      notifies :run, 'bash[redBorder_generate_server_key]', :delayed
-      notifies :run, 'bash[redBorder_update]', :delayed
-      notifies :run, 'bash[request_trial_license]', :delayed
+      # notifies :run, 'bash[request_trial_license]', :delayed
       only_if { ::File.exist?('/root/.upgrade-redborder-webui') }
       notifies :delete, 'file[/root/.upgrade-redborder-webui]', :immediately
       ignore_failure true
