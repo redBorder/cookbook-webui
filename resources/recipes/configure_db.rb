05c0b45c2bab22659578fd67ae187ee7ce4815f4
manegron <manegron@redborder.com>
2024-10-13 09:47:50 +0100

    remove configure_db and configure_server_key_trial_license

--- resources/recipes/configure_db.rb
+++
@@ -1,8 +0,0 @@
-# Cookbook:: webui
-# Recipe:: configure_db
-# Copyright:: 2024, redborder
-# License:: Affero General Public License, Version 3
-
-webui_config 'config' do
-  action [:configure_db]
-end
