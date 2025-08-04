d537aca7c46ce2832c47f6c81502980ac393dc69
Pablo Pérez <pperez@redborder.com>
2025-08-04 11:48:35 +0100

    Create blocked computers and start creating s3 malware buckets

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -21,6 +21,8 @@ action :add do
     user_sensor_map = new_resource.user_sensor_map
     web_dir = new_resource.web_dir
     s3_secrets = new_resource.s3_secrets
+    malware_enable = new_resource.malware_enable
+    policy_enforced = node["redborder"]["policy_enforced"]
 
     # INSTALLATION
     # begin
@@ -280,17 +282,19 @@ action :add do
     end
 
     # TEMPLATES
-    template '/var/www/rb-rails/config/aws.yml' do
-      source 'aws.yml.erb'
-      owner user
-      group group
-      mode '0644'
-      retries 2
-      cookbook 'webui'
-      variables(s3_local_storage: s3_local_storage, s3_bucket: s3_bucket, s3_host: s3_host,
-                s3_access_key: s3_access_key, s3_secret_key: s3_secret_key)
-      notifies :restart, 'service[webui]', :delayed unless node['redborder']['leader_configuring']
-      notifies :restart, 'service[rb-workers]', :delayed unless node['redborder']['leader_configuring']
+    [ "redborder", "malware" ].each do |bucket|
+      template "/var/www/rb-rails/config/aws#{ bucket == "redborder" ? "" : "_#{bucket}"}.yml" do
+        source 'aws.yml.erb'
+        owner user
+        group group
+        mode '0644'
+        retries 2
+        cookbook 'webui'
+        variables(s3_local_storage: s3_local_storage, s3_bucket: s3_bucket, s3_host: s3_host,
+                  s3_access_key: s3_access_key, s3_secret_key: s3_secret_key)
+        notifies :restart, 'service[webui]', :delayed unless node['redborder']['leader_configuring']
+        notifies :restart, 'service[rb-workers]', :delayed unless node['redborder']['leader_configuring']
+      end
     end
 
     template '/var/www/rb-rails/config/chef_config.yml' do
@@ -501,6 +505,19 @@ action :add do
       cookbook 'webui'
       variables(user_sensor_map: user_sensor_map)
       notifies :restart, 'service[webui]', :delayed unless node['redborder']['leader_configuring']
+    end
+
+    if malware_enable
+      template "/var/www/rb-rails/config/blocked_computers.yml" do
+        source "blocked_computers.yml.erb"
+        owner user
+        group group
+        mode 0644
+        retries 2
+        variables(conf: policy_enforced)
+      end
+    end
+
     end
 
     # LOG ROTATION
